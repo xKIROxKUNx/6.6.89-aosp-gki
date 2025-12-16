480883ff7061c72d7e2b481aa114308326a19b34
71chjzkc <71chjzkc@protonmail.com>
2025-12-15 23:14:32 -0300

    config: Bump to Capybara AOSP GKI 1.0

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Capybara Kernel
+kernel.string=Capybara 6.6.89 AOSP GKI 1.0
 do.devicecheck=0
 do.modules=0
 do.systemless=0
@@ -34,7 +34,7 @@ no_magisk_check=1
 
 kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
 
-ui_print "Flashing Cabybara Kernel..."
+ui_print "Flashing Capybara 6.6.89 AOSP GKI 1.0..."
 
 # boot install
 if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
